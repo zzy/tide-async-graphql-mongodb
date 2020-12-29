e3a810e9ed2afbc908491e8f5387fa3e88827e10
zzy <9809920@qq.com>
2020-12-29 22:35:51 +0800

    example

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,13 @@ async fn main() -> Result<(), std::io::Error> {
 
     let graphql_address = dotenv::var("GRAPHQL_ADDRESS").unwrap_or("0.0.0.0".to_owned());
     let graphql_port = dotenv::var("GRAPHQL_PORT").unwrap_or("8080".to_owned());
-    
+
     let mut app = tide::new();
-    
+
     app.at("/").get(|_| async { Ok("Hello, world!") });
 
-    app.listen(
-        format!("{}:{}", graphql_address, graphql_port)
-    ).await?;
+    app.listen(format!("{}:{}", graphql_address, graphql_port))
+        .await?;
 
     Ok(())
 }
