1bafbeb75f70406ff4c712a82166b6d549aa6a34
zzy <9809920@qq.com>
2021-01-15 13:14:59 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ async fn main() -> Result<(), std::io::Error> {
     // Initialize the application with state.
     let mut app = tide::with_state(State(gql::build_schema().await));
 
+
+    
     //environment variables defined in .env file
     app.at("/").get(tide::Redirect::new(CFG.get("GRAPHIQL_PATH").unwrap()));
     // app.at(ENV.get("GRAPHQL_PATH").unwrap()).post(async_graphql_tide::endpoint(schema));
