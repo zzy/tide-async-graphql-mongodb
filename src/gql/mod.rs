d3440297269566c0a7830060b890d37676aa31fe
zzy <9809920@qq.com>
2021-01-18 00:30:27 +0800

    handlebars

--- src/gql/mod.rs
+++ src/gql/mod.rs
@@ -19,7 +19,7 @@ pub async fn build_schema() -> Schema<QueryRoot, MutationRoot, EmptySubscription
     // get mongodb datasource. It can be added to:
     // 1. As global data for async-graphql.
     // 2. As application scope state of Tide
-    // 3. In product, I recommend lazy-static.rs.
+    // 3. Use lazy-static. In product, I recommend lazy-static.rs.
     let mongo_ds = mongo::DataSource::init().await;
 
     // The root object for the query and Mutatio, and use EmptySubscription.
@@ -28,22 +28,22 @@ pub async fn build_schema() -> Schema<QueryRoot, MutationRoot, EmptySubscription
     Schema::build(QueryRoot, MutationRoot, EmptySubscription).data(mongo_ds).finish()
 }
 
-pub async fn graphql(req: Request<State>) -> tide::Result<impl Into<Response>> {
+pub async fn graphql(req: Request<State>) -> tide::Result {
     let schema = req.state().0.clone();
     let gql_resp = schema.execute(receive_json(req).await?).await;
 
     let mut resp = Response::new(StatusCode::Ok);
     resp.set_body(Body::from_json(&gql_resp)?);
 
-    Ok(resp)
+    Ok(resp.into())
 }
 
-pub async fn graphiql(_: Request<State>) -> tide::Result<impl Into<Response>> {
+pub async fn graphiql(_: Request<State>) -> tide::Result {
     let mut resp = Response::new(StatusCode::Ok);
     resp.set_body(playground_source(GraphQLPlaygroundConfig::new(
         CFG.get("GRAPHQL_PATH").unwrap(),
     )));
     resp.set_content_type(mime::HTML);
 
-    Ok(resp)
+    Ok(resp.into())
 }
