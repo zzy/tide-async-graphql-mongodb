bdcf6126c1b008cb6be687c67696c24b3ced3a42
zzy <9809920@qq.com>
2021-05-31 18:25:26 +0800

    -

--- frontend-yew/src/main.rs
+++ frontend-yew/src/main.rs
@@ -1,52 +1,135 @@
-use yew::prelude::*;
+use serde::Deserialize;
+use yew::{
+    format::{Json, Nothing},
+    prelude::*,
+};
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
 
-enum Msg {
-    AddOne,
+#[derive(Deserialize, Debug, Clone)]
+pub struct ISSPosition {
+    latitude: String,
+    longitude: String,
 }
 
-struct Model {
-    // `ComponentLink` is like a reference to a component.
-    // It can be used to send messages to the component
+#[derive(Deserialize, Debug, Clone)]
+pub struct ISS {
+    message: String,
+    timestamp: i32,
+    iss_position: ISSPosition,
+}
+
+#[derive(Debug)]
+pub enum Msg {
+    GetLocation,
+    ReceiveResponse(Result<ISS, anyhow::Error>),
+}
+
+#[derive(Debug)]
+pub struct FetchServiceExample {
+    fetch_task: Option<FetchTask>,
+    iss: Option<ISS>,
     link: ComponentLink<Self>,
-    value: i64,
+    error: Option<String>,
 }
-
-impl Component for Model {
+/// Some of the code to render the UI is split out into smaller functions here to make the code
+/// cleaner and show some useful design patterns.
+impl FetchServiceExample {
+    fn view_iss_location(&self) -> Html {
+        match self.iss {
+            Some(ref space_station) => {
+                html! {
+                    <>
+                        <p>{ "The ISS is at:" }</p>
+                        <p>{ format!("Latitude: {}", space_station.iss_position.latitude) }</p>
+                        <p>{ format!("Longitude: {}", space_station.iss_position.longitude) }</p>
+                    </>
+                }
+            }
+            None => {
+                html! {
+                     <button onclick=self.link.callback(|_| Msg::GetLocation)>
+                         { "Where is the ISS?" }
+                     </button>
+                }
+            }
+        }
+    }
+    fn view_fetching(&self) -> Html {
+        if self.fetch_task.is_some() {
+            html! { <p>{ "Fetching data..." }</p> }
+        } else {
+            html! { <p></p> }
+        }
+    }
+    fn view_error(&self) -> Html {
+        if let Some(ref error) = self.error {
+            html! { <p>{ error.clone() }</p> }
+        } else {
+            html! {}
+        }
+    }
+}
+impl Component for FetchServiceExample {
     type Message = Msg;
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        Self { fetch_task: None, iss: None, link, error: None }
     }
+    fn change(&mut self, _props: Self::Properties) -> bool {
+        false
+    }
+    fn update(&mut self, msg: Self::Message) -> bool {
+        use Msg::*;
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
+            GetLocation => {
+                // 1. build the request
+                let request =
+                    Request::get("http://api.open-notify.org/iss-now.json")
+                        .body(Nothing)
+                        .expect("Could not build request.");
+                // 2. construct a callback
+                let callback = self.link.callback(
+                    |response: Response<Json<Result<ISS, anyhow::Error>>>| {
+                        let Json(data) = response.into_body();
+                        Msg::ReceiveResponse(data)
+                    },
+                );
+                // 3. pass the request and callback to the fetch service
+                let task = FetchService::fetch(request, callback)
+                    .expect("failed to start request");
+                // 4. store the task so it isn't canceled immediately
+                self.fetch_task = Some(task);
+                // we want to redraw so that the page displays a 'fetching...' message to the user
+                // so return 'true'
+                true
+            }
+            ReceiveResponse(response) => {
+                match response {
+                    Ok(location) => {
+                        self.iss = Some(location);
+                    }
+                    Err(error) => self.error = Some(error.to_string()),
+                }
+                self.fetch_task = None;
+                // we want to redraw so that the page displays the location of the ISS instead of
+                // 'fetching...'
                 true
             }
         }
     }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
-
     fn view(&self) -> Html {
         html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <>
+                { self.view_fetching() }
+                { self.view_iss_location() }
+                { self.view_error() }
+            </>
         }
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<FetchServiceExample>();
 }
