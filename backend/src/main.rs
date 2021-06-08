8107b714214614ab3c757c24f7f8b8f69a4de8e2
zzy <9809920@qq.com>
2021-06-08 17:24:00 +0800

    -

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,6 +5,9 @@ mod gql;
 mod users;
 mod projects;
 
+use tide::http::headers::HeaderValue;
+use tide::security::{CorsMiddleware, Origin};
+
 use crate::util::constant::CFG;
 use crate::gql::{build_schema, graphql, graphiql};
 
@@ -21,6 +24,13 @@ async fn main() -> Result<(), std::io::Error> {
     //environment variables defined in .env file
     app.at(CFG.get("GRAPHQL_PATH").unwrap()).post(graphql);
     app.at(CFG.get("GRAPHIQL_PATH").unwrap()).get(graphiql);
+
+    // suggest to specify urls
+    let cors = CorsMiddleware::new()
+        .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+        .allow_origin(Origin::from("*"))
+        .allow_credentials(false);
+    app.with(cors);
 
     app.listen(format!(
         "{}:{}",
